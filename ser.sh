24dacef04cfa189be38c40c31b81f0957eb84a63
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-06-12 16:07:25 +0000

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+sudo ufw allow 1789,1790,8000,22,80,443/tcp
+sudo ufw reload
+
 for(( i=1; i <=30; i++ ))
 do
 	cd ~
+	printf "\n\n"
+	printf "Restart ${nym}.service !"
 	kt='nym-mixnode'
 	nym=${kt}${i}
         sudo systemctl restart ${nym}.service
-	sleep 65
 	printf "\n\n"
 	printf "Xong ${nym}.service !"
 	printf "\n\n"
+	sleep 65	
 done
 printf "\n\n"
 printf "XONG !"
