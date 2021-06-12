d56a37c5af97d7ac794624f75cd36965fa6918ce
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-06-12 17:18:38 +0000

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -9,6 +9,20 @@ do
 	printf "\n\n"
 	kt='nym-mixnode'
 	nym=${kt}${i}
+	printf "Stop ${nym}.service !"	
+        sudo systemctl stop ${nym}.service
+	printf "\n\n"
+	printf "Xong ${nym}.service !"
+	printf "\n\n"
+	sleep 5	
+done
+
+for(( i=1; i <=30; i++ ))
+do
+	cd ~
+	printf "\n\n"
+	kt='nym-mixnode'
+	nym=${kt}${i}
 	printf "Restart ${nym}.service !"	
         sudo systemctl restart ${nym}.service
 	printf "\n\n"
