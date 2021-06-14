ade8ce902a0a64930fdb9f54729735f13fea4f3b
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-06-14 09:17:20 +0000

    Update upvi.sh

--- upvi.sh
+++ upvi.sh
@@ -8,7 +8,7 @@ do
 	kt='nym'
 	nym=${kt}${i}
 	rm -ri /home/${nym}/.nym/
-	cp -a /root/datavi/${nym}/ /home/${nym}/.nym/
+	cp -a /root/datavi/${nym}/.nym/ /home/${nym}/.nym/
 	sleep 2
 done
 printf "\n\n"
