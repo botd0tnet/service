e9dddc1a73da05268436ab63726e28bf1124f9a8
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-06-14 09:57:07 +0000

    Update downvi.sh

--- downvi.sh
+++ downvi.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+cd
 mkdir datavi
-cd datavi
+cd /root/datavi
 
 for(( i=1; i <=30; i++ ))
 do
-	kt='nym'
-	nym=${kt}${i}
-	mkdir ${nym}	
-	cp /home/${nym}/.nym/ /root/datavi/${nym}/
-	sleep 2
+        kt='nym'
+        nym=${kt}${i}
+        mkdir ${nym}
+        cp -a /home/${nym}/.nym/ /root/datavi/${nym}/
+        sleep 2
 done
 printf "\n\n"
 printf "DONE !"
